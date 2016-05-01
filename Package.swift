55be0ac3cafd73a728c3223ffd3458b36c32c7db
Yuki Takei <yuki@miketokyo.com>
2016-05-01 13:01:35 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "URI",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 6),
         .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 5),
         .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 6),
     ]
